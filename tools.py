32540add9dd6c8b2c818b030f1b5e0b0a7bd2162
gitenxx <giten@voidhimmel.com>
2020-08-02 05:56:38 +0300

    m

--- tools.py
+++ tools.py
@@ -1,7 +1,6 @@
 class TextHandler():
     @staticmethod
     def removeNestings(nested_list, output): 
-        # self = TextHandler()
         for i in nested_list: 
             if type(i) == list: 
                 TextHandler.removeNestings(i, output) 
